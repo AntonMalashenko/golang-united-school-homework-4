3cfbf26015a53f5f85c98338ef428e6b7d55e7c6
anton_malashenko <anton.mal.slb@gmail.com>
2022-05-16 13:05:58 +0300

    add check operands

--- string_sum.go
+++ string_sum.go
@@ -11,6 +11,19 @@ var (
 	errorEmptyInput = errors.New("input is empty")
 	// Use when the expression has number of operands not equal to two
 	errorNotTwoOperands = errors.New("expecting two operands, but received more or less")
+
+	allowedOperands = map[rune]struct{}{
+		'0': struct{}{},
+		'1': struct{}{},
+		'2': struct{}{},
+		'3': struct{}{},
+		'4': struct{}{},
+		'5': struct{}{},
+		'6': struct{}{},
+		'7': struct{}{},
+		'8': struct{}{},
+		'9': struct{}{},
+	}
 )
 
 // Implement a function that computes the sum of two int numbers written as a string
@@ -23,10 +36,33 @@ var (
 //
 // Use the errors defined above as described, again wrapping into fmt.Errorf
 
+func checkOperands(input string) (res bool) {
+	limit := 2
+	exists := 0
+	for _, v := range input {
+		_, ok := allowedOperands[v]
+		if ok {
+			exists++
+			if exists > limit {
+				return false
+			}
+		}
+	}
+	if exists < limit {
+		return false
+	}
+	return true
+}
+
 func StringSum(input string) (output string, err error) {
 	input = strings.TrimSpace(input)
+
 	if len(input) == 0 {
 		return output, errorEmptyInput
 	}
+	if !checkOperands(input) {
+		return output, errorNotTwoOperands
+	}
+
 	return "", nil
 }
